1c6c4969ab662d6a8fe017424ade1c5171fe2154
Joestarfish <130308219+Joestarfish@users.noreply.github.com>
2023-08-27 20:41:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# FarmChest
+
+A PMMP plugin that modifies the Trapped Chest to make it harvest nearby crops
+
+[![](https://poggit.pmmp.io/shield.state/FarmChest)](https://poggit.pmmp.io/p/FarmChest) [![](https://poggit.pmmp.io/shield.dl.total/FarmChest)](https://poggit.pmmp.io/p/FarmChest)
+
+The following crops will be harvested:
+
+![wheat](https://github.com/ZtechNetwork/MCBVanillaResourcePack/blob/master/textures/items/wheat.png?raw=true)
+![beetroot](https://github.com/ZtechNetwork/MCBVanillaResourcePack/blob/master/textures/items/beetroot.png?raw=true)
+![potato](https://github.com/ZtechNetwork/MCBVanillaResourcePack/blob/master/textures/items/potato.png?raw=true)
+![carrot](https://github.com/ZtechNetwork/MCBVanillaResourcePack/blob/master/textures/items/carrot.png?raw=true)
+![pumpkin_face_off](https://github.com/ZtechNetwork/MCBVanillaResourcePack/blob/master/textures/blocks/pumpkin_face_off.png?raw=true)
+![melon](https://github.com/ZtechNetwork/MCBVanillaResourcePack/blob/master/textures/items/melon.png?raw=true)
+![sweet_berries](https://github.com/ZtechNetwork/MCBVanillaResourcePack/blob/master/textures/items/sweet_berries.png?raw=true)
+
+<details>
+	<ul>
+		<li>Wheat</li>
+		<li>Beetroots</li>
+		<li>Potatoes</li>
+		<li>Carrots</li>
+		<li>Pumpkin</li>
+		<li>Melon</li>
+		<li>Sweet berries</li>
+	</ul>
+</details>
+
+You could also use a plugin such as [VanillaHopper](https://poggit.pmmp.io/p/VanillaHopper/) that implements hopper. They will pick items inside of the chest and move them around your farm
+
+# Usage
+
+To use the FarmChest, you can simply right click on it and it will harvest crops in a specified zone.
+
+You can also sneak while clicking on the chest to make it harvest and keeping it closed
+
+# Configuration
+
+Inside of the `plugin_data/FarmChest/config.yml` file, you may change the following:
+
+-   **range** - The range at which the FarmChest will harvest crops
+    -   **x-axis** - The number of blocks that will be checked on the X axis
+    -   **z-axis** - The number of blocks that will be checked on the Z axis
+-   **should-drop-leftover-items** - Should the FarmChest drop the items that weren't able to fit inside of it ?
+-   **can-player-pickup-block** - Can the player destroy and pickup the FarmChest ?
---
+++ src/Main.php
@@ -0,0 +1,186 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Joestarfish\FarmChest;
+
+use pocketmine\block\Block;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\block\Crops;
+use pocketmine\block\inventory\ChestInventory;
+use pocketmine\block\SweetBerryBush;
+use pocketmine\block\tile\Chest as TileChest;
+use pocketmine\block\VanillaBlocks;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\inventory\InventoryTransactionEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\inventory\transaction\action\CreateItemAction;
+use pocketmine\inventory\transaction\InventoryTransaction;
+use pocketmine\item\Item;
+use pocketmine\item\VanillaItems;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\inventory\transaction\action\InventoryAction;
+use pocketmine\inventory\transaction\action\SlotChangeAction;
+
+class Main extends PluginBase implements Listener {
+	private Config $config;
+
+	public function onEnable(): void {
+		$this->config = $this->getConfig();
+		$this->getServer()
+			->getPluginManager()
+			->registerEvents($this, $this);
+	}
+
+	public function onInteract(PlayerInteractEvent $event) {
+		if ($event->getAction() != PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
+			return;
+		}
+
+		if ($event->getBlock()->getTypeId() != BlockTypeIds::TRAPPED_CHEST) {
+			return;
+		}
+
+		$position = $event->getBlock()->getPosition();
+		$world = $position->getWorld();
+		$tile = $world->getTile($position);
+
+		if (!$tile instanceof TileChest) {
+			return;
+		}
+
+		$inventory = $tile->getInventory();
+
+		$leftover_drops = [];
+
+		$max_x = (int) abs($this->config->getNested('range.x-axis', 5));
+		// We could add the Y axis
+		$max_z = (int) abs($this->config->getNested('range.z-axis', 5));
+
+		for ($x = -$max_x; $x < $max_x; $x++) {
+			for ($z = -$max_z; $z < $max_z; $z++) {
+				$target = $world->getBlockAt(
+					$position->getFloorX() + $x,
+					$position->getFloorY(),
+					$position->getFloorZ() + $z,
+				);
+
+				if (!$this->isValidBlock($target)) {
+					continue;
+				}
+
+				$drops = $this->getDrops($target);
+
+				if (count($drops) == 0) {
+					continue;
+				}
+
+				$leftover_drops = array_merge(
+					$leftover_drops,
+					$inventory->addItem(...$drops),
+				);
+			}
+		}
+
+		// Without a transaction, hopper plugins might not notice that new items were added to the chest
+		// This isn't actually what happens but i don't think it will cause issues
+		$transaction = new InventoryTransaction($event->getPlayer(), [
+			new SlotChangeAction(
+				$inventory,
+				0,
+				$inventory->getItem(0),
+				$inventory->getItem(0),
+			),
+		]);
+		$ev = new InventoryTransactionEvent($transaction);
+		$ev->call();
+
+		if (!$this->config->get('should-drop-leftover-items', true)) {
+			return;
+		}
+
+		foreach ($leftover_drops as $item) {
+			$world->dropItem($position, $item);
+		}
+	}
+
+	public function onBreak(BlockBreakEvent $event) {
+		if ($event->getBlock()->getTypeId() != BlockTypeIds::TRAPPED_CHEST) {
+			return;
+		}
+
+		if ($this->config->get('can-player-pickup-block', true)) {
+			return;
+		}
+
+		// The content of the chest will still be dropped which is what we want
+		$event->setDrops([]);
+	}
+
+	private function isValidBlock(Block $block): bool {
+		$valid_crops = [
+			BlockTypeIds::WHEAT,
+			BlockTypeIds::BEETROOTS,
+			BlockTypeIds::POTATOES,
+			BlockTypeIds::CARROTS,
+			BlockTypeIds::PUMPKIN,
+			BlockTypeIds::MELON,
+			BlockTypeIds::SWEET_BERRY_BUSH,
+		];
+
+		if (!in_array($block->getTypeId(), $valid_crops)) {
+			return false;
+		}
+
+		if ($block instanceof Crops && $block->getAge() < $block::MAX_AGE) {
+			return false;
+		}
+
+		if (
+			$block instanceof SweetBerryBush &&
+			$block->getAge() < $block::STAGE_BUSH_SOME_BERRIES
+		) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @return Item[]
+	 */
+	private function getDrops(Block $block): array {
+		$position = $block->getPosition();
+		$world = $position->getWorld();
+
+		if ($block instanceof SweetBerryBush) {
+			if (($count = $block->getBerryDropAmount()) < 1) {
+				return [];
+			}
+
+			$drops = [VanillaItems::SWEET_BERRIES()->setCount($count)];
+
+			$world->setBlockAt(
+				$position->getFloorX(),
+				$position->getFloorY(),
+				$position->getFloorZ(),
+				$block->setAge($block::STAGE_BUSH_NO_BERRIES),
+			);
+
+			return $drops;
+		}
+
+		$drops = $block->getDropsForCompatibleTool(VanillaItems::AIR());
+
+		$world->setBlockAt(
+			$position->getFloorX(),
+			$position->getFloorY(),
+			$position->getFloorZ(),
+			$block instanceof Crops ? $block->setAge(0) : VanillaBlocks::AIR(),
+		);
+
+		return $drops;
+	}
+}
